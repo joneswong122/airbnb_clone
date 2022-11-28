f144326ed2ed3eab181c919c1852e6c381306826
joneswong <jones_wong@icloud.com>
2022-11-28 16:59:32 +0000

    add new flat done

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -9,14 +9,17 @@ class FlatsController < ApplicationController
   end
 
   def create
+    @user = current_user
     @flat = Flat.new(strong_params)
+    @flat.user = @user
     @flat.save
+    redirect_to flats_path
     # redirect_to flat_path(@flat)
   end
 
   private
 
   def strong_params
-    params.require(:flat).permit(:id, :price, :address, :room_type, :total_occupancy)
+    params.require(:flat).permit(:user_id, :price, :address, :room_type, :total_occupancy)
   end
 end
