dc2bb84b6e9c03fd87dd7c86edd215671f393fa8
Daniel Santos <danielsantos.wd@gmail.com>
2022-12-01 17:36:42 +0000

    seed title

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,12 +21,12 @@ puts "#{first_user.email} is created"
 
 puts "Creating flats..."
 
-flat1 = { user: first_user, price: "100", title: "Flat 1", city: "London", address: "7 Boundary St, London E2 7JE", total_occupancy: "1", room_type: "single"  }
-flat2 = { user: first_user, price: "102", title: "Flat 2", city: "London", address: "56A Shoreditch High St, London E1 6PQ", total_occupancy: "2", room_type: "double" }
-flat3 = { user: first_user, price: "105", title: "Flat 3", city: "Paris", address: "75008 Paris", total_occupancy: "4", room_type: "single" }
-flat4 =  { user: first_user, price: "250", title: "Flat 4", city: "Southampton", address: "High St, Southampton", total_occupancy: "5", room_type: "double" }
-flat5 =  { user: first_user, price: "200", title: "Flat 5", city: "Menchester", address: "High St, Menchester", total_occupancy: "6", room_type: "family" }
-flat6 =  { user: first_user, price: "205", title: "Flat 6", city: "London", address: "High St, london", total_occupancy: "4", room_type: "family" }
+flat1 = { user: first_user, price: "100", title: "You are on Right Place", city: "London", address: "7 Boundary St, London E2 7JE", total_occupancy: "1", room_type: "single"  }
+flat2 = { user: first_user, price: "102", title: "One Place, Many Things", city: "London", address: "56A Shoreditch High St, London E1 6PQ", total_occupancy: "2", room_type: "double" }
+flat3 = { user: first_user, price: "105", title: "Happy People, Happy Living", city: "Paris", address: "75008 Paris", total_occupancy: "4", room_type: "single" }
+flat4 =  { user: first_user, price: "250", title: "The joy of Living at Best", city: "Southampton", address: "High St, Southampton", total_occupancy: "5", room_type: "double" }
+flat5 =  { user: first_user, price: "200", title: "Let Living at Right Place", city: "Menchester", address: "High St, Menchester", total_occupancy: "6", room_type: "family" }
+flat6 =  { user: first_user, price: "205", title: "The dreamy atmosphere", city: "London", address: "High St, london", total_occupancy: "4", room_type: "family" }
 
 [flat1, flat2, flat3, flat4, flat5, flat6].each do |attributes|
   file = URI.open("https://lh3.googleusercontent.com/blogger_img_proxy/ANbyha2ZclDOOI2jWMayud6MxiHtrzBB5oyNHVXYIw0hkncOCHnHbsjQxaAg16Yb6AQ3kMKhPvZ5n0lv9E6ugFvDKoc8oMgCetuioqAslhtte7FvMwN8gif8h-4BbeXMAsRjZ7Ip4Lhx-xLk3FAalpI99wIGbnF7g4yD31_k0dfL=s0-d")
