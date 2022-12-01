ed70b61ebfb9b71fe915c8471eb0ee78b5f22b86
Arbi Velaj <arbi.velaj@gmail.com>
2022-12-01 13:16:59 +0000

    fix flatpickr

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,14 +1,16 @@
-import { Controller } from "@hotwired/stimulus";
+import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
+// Connects to data-controller="flatpickr"
 export default class extends Controller {
-  static targets = [ "startTime", "endTime" ]
+  // Inform the controller that it has two targets in the form, which are our inputs
+
+  static get targets() {
+    return [ "startTime", "endTime" ]
+  }
 
   connect() {
-    flatpickr(this.startTimeTarget, {enableTime: true,
-      // Provide an id for the plugin to work
-      plugins: [new rangePlugin({ input: "#end_time"})]})
+    // flatpickr(this.startTimeTarget, {})
     flatpickr(this.endTimeTarget, {})
   }
 }
